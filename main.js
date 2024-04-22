844be178fa2c75e136da03a8c6e4b28a84781934
Abubakar <abubakarsiddikl.info@gmail.com>
2024-04-22 14:30:45 +0600

    flipped issue

--- main.js
+++ main.js
@@ -1,22 +1,35 @@
 document.addEventListener("DOMContentLoaded", function() {
-    function copyTextAndShowMessage(copyText, element) {
-      navigator.clipboard.writeText(copyText).then(function() {
-        // Temporarily replace the text content with "Text copied"
-        var originalText = element.textContent;
-        element.textContent = "Text copied";
-        setTimeout(function() {
-          element.textContent = originalText;
-        }, 1000);
-      }, function(err) {
-        console.error('Unable to copy: ', err);
-      });
-    }
+  var getDemoBtns = document.querySelectorAll('.get-demo-btn');
+  var flippedCard = null;
 
-    var copyIcons = document.querySelectorAll(".copyicon");
-    copyIcons.forEach(function(icon) {
-      icon.addEventListener("click", function() {
-        var copyText = this.previousElementSibling.textContent;
-        copyTextAndShowMessage(copyText, this.previousElementSibling);
-      });
+  getDemoBtns.forEach(function(btn) {
+    btn.addEventListener('click', function() {
+      var card = this.closest('.flip-card');
+      if (flippedCard && flippedCard !== card) {
+        flippedCard.classList.remove('flipped');
+      }
+      card.classList.toggle('flipped');
+      flippedCard = card;
     });
+  });
+
+  function copyTextAndShowMessage(copyText, element) {
+    navigator.clipboard.writeText(copyText).then(function() {
+      var originalText = element.textContent;
+      element.textContent = "Text copied";
+      setTimeout(function() {
+        element.textContent = originalText;
+      }, 1000);
+    }, function(err) {
+      console.error('Unable to copy: ', err);
+    });
+  }
+
+  var copyIcons = document.querySelectorAll(".copyicon");
+  copyIcons.forEach(function(icon) {
+    icon.addEventListener("click", function() {
+      var copyText = this.previousElementSibling.textContent;
+      copyTextAndShowMessage(copyText, this.previousElementSibling);
+    });
+  });
 });
